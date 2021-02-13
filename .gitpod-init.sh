4128543e0e3f8962af8492e69dec34766f601766
Scott Collier <scott@onlinespaces.com>
2021-02-13 18:57:44 +0000

    Add git prompt

--- .gitpod-init.sh
+++ .gitpod-init.sh
@@ -3,3 +3,5 @@ cp env.example .env
 composer install
 export PATH="$PATH:/workspace/drupalize/vendor/bin"
 apachectl start
+cp 10-git-prompt ~/.bashrc.d
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
