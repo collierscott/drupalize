3ad40af882513ccd19d80628ecabbe2bd2d13eae
Scott Collier <scott@onlinespaces.com>
2021-02-14 17:58:30 +0000

    Add backup and migrate

--- .gitpod-init.sh
+++ .gitpod-init.sh
@@ -1,9 +1,9 @@
 mysql -u root -e "create database drupalize"
 cp env.example .env
-composer install
-export PATH="$PATH:/workspace/drupalize/vendor/bin"
 apachectl start
 cp 10-git-prompt ~/.bashrc.d
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 source ~/.bashrc
-drush cim
+composer install
+export PATH="$PATH:/workspace/drupalize/vendor/bin"
+/workspace/drupalize/vendor/bin/drush cim
